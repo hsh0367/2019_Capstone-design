521381bf14de01041a5d07ae9470dfca73dfabf3
강신원 <wnfansl788@gmail.com>
2019-10-17 17:25:27 +0900

    ADD :  draw canvas, data preprocessing #3 #4

--- quickdraw/dataset.py
+++ quickdraw/dataset.py
@@ -1,8 +1,12 @@
 import struct
 from struct import unpack
-import numpy as np
 import matplotlib.pyplot as plt
-import pylab
+from matplotlib import transforms
+import sys
+import os
+import errno
+import multiprocessing
+from mpl_toolkits.mplot3d import axes3d
 
 
 def unpack_drawing(file_handle):
@@ -37,12 +41,74 @@ def unpack_drawings(filename):
                 break
 
 
-for drawing in unpack_drawings('airplane.bin'):
-    image_array = drawing['image']
-    image = np.zeros((256, 256))
+def data_preprocessing(filename, path):
+    _dpi = 100
+    count = 0
+    data_file_name = filename + ".bin"
+    for drawing in unpack_drawings(data_file_name):
+        image_array = drawing['image']
+        plt.clf()
+        plt.figure(figsize=(331 / _dpi, 333 / _dpi), dpi=_dpi)
+        for x in range(len(image_array)):
+            x1, y1 = image_array[x][0], image_array[x][1]
+            plt.plot(x1, y1, color='k')
 
-    for x in range(len(image_array)):
-        x1, y1 = image_array[x][0], image_array[x][1]
-        plt.plot(x1, y1, color='k')
+        save_path = path + str(count) + ".png"
         plt.axis('off')
-        pylab.savefig('mypic.png', bbox_inches='tight', pad_inches=0)
+        plt.savefig(save_path, bbox_inches='tight', pad_inches=0, dpi=_dpi)
+        plt.close()
+        print("save png:", save_path)
+        count += 1
+
+
+def is_list(files):
+    if type(files) == str:
+        load_binfile(files)
+    else:
+        for i in range(len(files)):
+            load_binfile(files[i])
+
+
+def load_binfile(file_name):
+    data_folder_name = file_name
+    data_file_name = file_name + ".bin"
+    data_folder_path = "./" + data_folder_name + "/"
+    try:
+        os.makedirs(data_folder_path)
+        print('mkdir -p', data_folder_path)
+        data_processing(file_name, data_folder_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(data_folder_path + "/" + data_folder_name + "/"):
+            print('already exist folder')
+            data_processing(data_file_name)
+            pass
+        else:
+            raise
+
+
+def get_process_schedule():
+    process_count = 4
+    data_count = len(sys.argv) - 1
+    list = []
+    if data_count < 5:
+        for i in range(data_count):
+            list.append(sys.argv[i + 1])
+        return list
+    else:
+        for i in range(process_count):
+            list.append([])
+        for i in range(data_count):
+            list[i % 4].append(sys.argv[i + 1])
+        return list
+
+
+if __name__ == "__main__":
+    process = []
+    for files in get_process_schedule():
+        p = multiprocessing.Process(target=load_binfile, args=(files,))
+        process.append(p)
+        p.start()
+
+    for p in process:
+        p.join()
+
